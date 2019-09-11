2f671f8f7f09896488d40e8ec09c05862401fb1d
Terashima <yamanogomora@gmail.com>
2019-09-11 17:28:51 +0900

    Add: #1 MVCデザインパターンで作成するためのセットアップとModel層の作成

---
+++ GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by 寺島 洋平 on 2019/09/11.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
---
+++ GoodEatins/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by 寺島 洋平 on 2019/09/11.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
