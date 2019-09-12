f593977105cc63bfa250000331f647d9147af1be
Terashima <yamanogomora@gmail.com>
2019-09-12 17:16:51 +0900

    Add: #2 レシピ詳細画面を管理するクラスファイルを作成して、レシピ詳細画面のUIをレイアウト

---
+++ GoodEatins/Controller/RecipeDetailVC.swift
@@ -0,0 +1,28 @@
+//
+//  RecipeDetailVC.swift
+//  GoodEatins
+//
+//  Created by 寺島 洋平 on 2019/09/12.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailVC: UIViewController {
+
+    @IBOutlet weak var recipeImg: UIImageView!
+    @IBOutlet weak var recipeTitle: UILabel!
+    @IBOutlet weak var recipeInstructions: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
